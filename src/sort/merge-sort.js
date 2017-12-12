822c36978896dd80f0dd898a8e5bc4de295d9ef8
mimi4Real <706649059@qq.com>
2017-12-12 09:59:31 +0800

    update mergeSort

--- src/sort/merge-sort.js
+++ src/sort/merge-sort.js
@@ -7,26 +7,60 @@
 
 class MergeSort{
 	mergeSort(arr){
+
+		let merge = (left, right) => {
+			var final = [];
+
+			while (left.length && right.length) {
+				if (left[0] < right[0])
+					final.push(left.shift());
+				else
+					final.push(right.shift());
+			}
+
+			return final.concat(left, right);
+		}
+
+
 		if(arr.length === 1) return arr;
 
 		let mid = ~~(arr.length/2);
 
-		return this.merge(this.mergeSort(arr.slice(0, mid)), this.mergeSort(arr.slice(mid)))
+		return merge(this.mergeSort(arr.slice(0, mid)), this.mergeSort(arr.slice(mid)))
 	}
 
-	merge(left, right){
-		var final = [];
-
-		while (left.length && right.length) {
-			if (left[0] < right[0])
-				final.push(left.shift());
-			else
-				final.push(right.shift());
+	mergeSort2(arr, result, start, end){
+		let merge = (arr, result, leftStart, leftEnd, rightStart, rightEnd, start)=>{
+			let k = start;
+			while (leftStart <= leftEnd && rightStart <= rightEnd)
+				result[k++] = arr[leftStart] < arr[rightStart] ? arr[leftStart++] : arr[rightStart++];
+			while (leftStart <= leftEnd)
+				result[k++] = arr[leftStart++];
+			while (rightStart <= rightEnd)
+				result[k++] = arr[rightStart++];
+			for (k = start; k <= end; k++)
+				arr[k] = result[k];
 		}
 
-		return final.concat(left, right);
+		if (start >= end)
+			return;
+
+		let len = end - start, mid = (len >> 1) + start; // len >> 1 ===  ~~(len/2)
+
+		let leftStart = start, leftEnd = mid;
+		let rightStart = mid + 1, rightEnd = end;
+		this.mergeSort2(arr, result, leftStart, leftEnd);
+		this.mergeSort2(arr, result, rightStart, rightEnd);
+
+		merge(arr, result, leftStart, leftEnd, rightStart, rightEnd, start);
 	}
 }
 
 const obj = new MergeSort();
-console.log(obj.mergeSort([45,1,1,9,2,5,23,72,3]));
+const arr = [45,1,1,9,2,5,23,72,3];
+
+// console.log(obj.mergeSort(arr))
+
+let result = []
+obj.mergeSort2(arr, result, 0 , arr.length - 1);
+console.log(result)
