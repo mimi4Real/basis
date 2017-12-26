f13091939bc15de0f13c6002b8a06192ac440d5d
mimi4Real <706649059@qq.com>
2017-12-26 17:24:11 +0800

    add MDR

--- src/sort/expand/tree-traversal.js
+++ src/sort/expand/tree-traversal.js
@@ -16,20 +16,46 @@ function build(nodes) {
 	}
 }
 
-function LDR(node){
+//中序遍历
+function MDR(node) {
+	let stack = [];
+
+	while(node || stack.length){
+		while (node) {
+			stack.push(node);
+			node = node.left;
+		}
+
+		while(stack.length && !node){
+			node = stack.pop();
+			console.log(node.value);
+			node = node.right
+		}
+	}
+}
+
+//中序遍历（递归）
+function MDR_recursion(node){
 	if (node.left) {
-		console.log(node.left)
-		LDR(node.left);
+		// console.log(node.left)
+		MDR_recursion(node.left);
 	};
-	console.log(node.value);
+
 	if (node.right) {
-		console.log(node.right)
-		LDR(node.right);
+		// console.log(node.right)
+		MDR_recursion(node.right);
 	};
+
+	console.log(node.value);
+
 }
 
 
-let array = [23, 5, 67, 212, 34]
+let array = [23, 5, 67, 212, 34]  //完全二叉树
+//前序：23 5 212 34 67
+//中序：212 5 34 23 67
+//后序：212 34 5 67 23
+
 let nodes = [];
 array.forEach(value => {
 	let treeObj = new tree();
@@ -39,4 +65,5 @@ array.forEach(value => {
 
 build(nodes); // tree build
 
-LDR(nodes[0]); //inorder traversal
+MDR_recursion(nodes[0]);
+MDR(nodes[0]);
